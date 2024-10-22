62782d29144b795d49c6bad759db0c606d363a8c
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-10-22 22:07:18 +0200

    Render number of events that fit Date

--- src/components/Date/DateOfMonth.tsx
+++ src/components/Date/DateOfMonth.tsx
@@ -6,37 +6,52 @@ import { useCallback, useEffect, useRef, useState } from "react";
 
 type DateofMonthProps = {
 	year: number;
-	month: number;  
+	month: number;
 	date: number;
 	dayClass?: string;
-	setModalOpen:  React.Dispatch<React.SetStateAction<boolean>>;
+	setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 	setSelectedDate: React.Dispatch<React.SetStateAction<SelectedDate>>;
 	setIsEditing: React.Dispatch<React.SetStateAction<Event | null>>;
-}
-
+};
 
 // COMPONENT TO CONTAIN DATE AND EVENT-LISININGS
-const DateOfMonth: React.FC<DateofMonthProps> = ({ year, month, date, dayClass, setModalOpen, setSelectedDate, setIsEditing }) => {
+const DateOfMonth: React.FC<DateofMonthProps> = ({
+	year,
+	month,
+	date,
+	dayClass,
+	setModalOpen,
+	setSelectedDate,
+	setIsEditing,
+}) => {
 	const { eventsArray } = useEventContext();
-	const [eventsToDisplay, setEventsToDisplay] = useState(getAndSortEvents(eventsArray))
-	const [eventsToFitDate, setEventsToFitDate] = useState<number>(eventsArray.length);
+	const [eventsToDisplay, setEventsToDisplay] = useState(
+		getAndSortEvents(eventsArray)
+	);
+	const [eventsToFitDate, setEventsToFitDate] = useState<number>(
+		eventsArray.length
+	);
 	const [eventsOverflowed, setEventsOverFlowed] = useState(0);
 	const dateRef = useRef<HTMLDivElement | null>(null);
-	
+
 	// GET BOOLEAN TO RENDER TODAY STYLE
-	const getIfIsToday = useCallback(()=> {
-		const today = new Date()
-		if(today.getFullYear() === year && today.getMonth() === month && today.getDate() === date) {
+	const getIfIsToday = useCallback(() => {
+		const today = new Date();
+		if (
+			today.getFullYear() === year &&
+			today.getMonth() === month &&
+			today.getDate() === date
+		) {
 			return true;
 		} else {
 			return false;
-		};
+		}
 	}, [date, month, year]);
 
 	const isToday = getIfIsToday();
 
-	// GET THE DAY OF THE WEEK 
-	const dayOfWeek = new Date(year, month, date).getDay();	
+	// GET THE DAY OF THE WEEK
+	const dayOfWeek = new Date(year, month, date).getDay();
 
 	const days = [
 		"Sunday",
@@ -45,87 +60,100 @@ const DateOfMonth: React.FC<DateofMonthProps> = ({ year, month, date, dayClass, 
 		"Wednesday",
 		"Thursday",
 		"Friday",
-		"Saturday"
+		"Saturday",
 	];
 
-		// LIMIT EVENTS TO DISPLAY TO AVOID OVERFLOW
-		useEffect(()=> {
-			const limitEventsToDisplay = ()=> {
-				let eventsOfTheDate = getAndSortEvents(eventsArray)
-				if(eventsOfTheDate.length - eventsToFitDate > 0) {
-					setEventsOverFlowed(eventsOfTheDate.length - eventsToFitDate);
-					eventsOfTheDate = eventsOfTheDate.slice(0, eventsToFitDate);
-				}
-				setEventsToDisplay(eventsOfTheDate)
-				
+	// LIMIT EVENTS TO DISPLAY TO AVOID OVERFLOW
+	useEffect(() => {
+		const limitEventsToDisplay = () => {
+			let eventsOfTheDate = getAndSortEvents(eventsArray);
+			if (eventsOfTheDate.length - eventsToFitDate > 0) {
+				setEventsOverFlowed(eventsOfTheDate.length - eventsToFitDate);
+				eventsOfTheDate = eventsOfTheDate.slice(0, eventsToFitDate);
 			}
-		
-			limitEventsToDisplay();
-	
-		}, [eventsArray, eventsToFitDate])
+			setEventsToDisplay(eventsOfTheDate);
+		};
+
+		limitEventsToDisplay();
+	}, [eventsArray, eventsToFitDate]);
 
 	// CALCULATE EVENTS TO FIT IN DATE SQUARE
-	useEffect(()=> {
+	useEffect(() => {
 		let currentHeight;
-		const getDateSquareHeight = ()=> {
-			if(dateRef.current) {
-				currentHeight = dateRef.current.offsetHeight;	
-				const eventHeight = 24;				
-				const eventsToFit = Math.floor(currentHeight / (eventHeight + 16));
+		const getDateSquareHeight = () => {
+			if (dateRef.current) {
+				currentHeight = dateRef.current.offsetHeight;
+				const eventHeight = 24;
+				const eventsToFit = Math.floor(
+					currentHeight / (eventHeight + 16)
+				);
 				// TODO: RERENDERS ON EVERY RESIZE!!
 				if (eventsToFit !== eventsToFitDate) {
 					console.log(eventsToFit);
 					setEventsToFitDate(eventsToFit);
-				}					
-			} 
-		} 
+				}
+			}
+		};
 
 		getDateSquareHeight();
 		window.addEventListener("resize", getDateSquareHeight);
 
 		return () => window.removeEventListener("resize", getDateSquareHeight);
-	}, [])
+	}, []);
 
 	// GET EVENTS OF THE DAY
 	function getAndSortEvents(eventsArray: Event[]) {
 		const eventsOfTheDate: Event[] = eventsArray.filter((event: Event) => {
-			const eventDate = new Date(year, month, date).toLocaleDateString();		
-			return (
-				eventDate === event.date
-			);
+			const eventDate = new Date(year, month, date).toLocaleDateString();
+			return eventDate === event.date;
 		});
 
-		const sortedEventsArray = eventsOfTheDate.sort((a, b)=> 			
-			Number(a.startTime.replace(":","")) - Number(b.startTime.replace(":",""))
-		)
-		
-		return sortedEventsArray
+		const sortedEventsArray = eventsOfTheDate.sort(
+			(a, b) =>
+				Number(a.startTime.replace(":", "")) -
+				Number(b.startTime.replace(":", ""))
+		);
+
+		return sortedEventsArray;
 	}
-
-
-	
 
 	const handleAddEvent = (year: number, month: number, date: number) => {
-		setModalOpen(true)
-		setSelectedDate({ year, month, date })
-	}
+		setModalOpen(true);
+		setSelectedDate({ year, month, date });
+	};
 
-  return (
-	<div ref={dateRef} className={`day ${dayClass}`}>
-		    <div className="day-header">
-              <div className="week-name">{days[dayOfWeek]}</div>
-              <div className={`day-number ${isToday && "today"}`}>{date}</div>
-              <button className="add-event-btn" onClick={()=>handleAddEvent(year, month, date)}>+</button>
-            </div>
+	return (
+		<div ref={dateRef} className={`day ${dayClass}`}>
+			<div className="day-header">
+				<div className="week-name">{days[dayOfWeek]}</div>
+				<div className={`day-number ${isToday && "today"}`}>{date}</div>
+				<button
+					className="add-event-btn"
+					onClick={() => handleAddEvent(year, month, date)}
+				>
+					+
+				</button>
+			</div>
 
 			<div className="events">
-				{eventsToDisplay.map((event, index) => 
-					<EventListing key={index} event={event} setIsEditing={setIsEditing} setModalOpen={setModalOpen}/>	
-				)}
+				{eventsToDisplay.map((event, index) => (
+					<EventListing
+						key={index}
+						event={event}
+						setIsEditing={setIsEditing}
+						setModalOpen={setModalOpen}
+					/>
+				))}
 			</div>
-			{eventsOverflowed ? <button className="events-view-more-btn">+ {eventsOverflowed} more</button>: ""}
-	</div>
-  )
-}
+			{eventsOverflowed ? (
+				<button className="events-view-more-btn">
+					+ {eventsOverflowed} more
+				</button>
+			) : (
+				""
+			)}
+		</div>
+	);
+};
 
 export default DateOfMonth;
