3a54a0fd4bd7dfd5749d6786abd85c25b2796f65
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-10-22 22:04:42 +0200

    Render number of events that fit Date

--- src/components/Date/DateOfMonth.tsx
+++ src/components/Date/DateOfMonth.tsx
@@ -2,7 +2,7 @@ import { SelectedDate } from "../Calendar/Calendar";
 import EventListing from "../EventListing/EventListing";
 import type { Event } from "../../hooks/useEvent";
 import { useEventContext } from "../../hooks/useEventContext";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 type DateofMonthProps = {
 	year: number;
@@ -18,6 +18,10 @@ type DateofMonthProps = {
 // COMPONENT TO CONTAIN DATE AND EVENT-LISININGS
 const DateOfMonth: React.FC<DateofMonthProps> = ({ year, month, date, dayClass, setModalOpen, setSelectedDate, setIsEditing }) => {
 	const { eventsArray } = useEventContext();
+	const [eventsToDisplay, setEventsToDisplay] = useState(getAndSortEvents(eventsArray))
+	const [eventsToFitDate, setEventsToFitDate] = useState<number>(eventsArray.length);
+	const [eventsOverflowed, setEventsOverFlowed] = useState(0);
+	const dateRef = useRef<HTMLDivElement | null>(null);
 	
 	// GET BOOLEAN TO RENDER TODAY STYLE
 	const getIfIsToday = useCallback(()=> {
@@ -44,8 +48,46 @@ const DateOfMonth: React.FC<DateofMonthProps> = ({ year, month, date, dayClass, 
 		"Saturday"
 	];
 
+		// LIMIT EVENTS TO DISPLAY TO AVOID OVERFLOW
+		useEffect(()=> {
+			const limitEventsToDisplay = ()=> {
+				let eventsOfTheDate = getAndSortEvents(eventsArray)
+				if(eventsOfTheDate.length - eventsToFitDate > 0) {
+					setEventsOverFlowed(eventsOfTheDate.length - eventsToFitDate);
+					eventsOfTheDate = eventsOfTheDate.slice(0, eventsToFitDate);
+				}
+				setEventsToDisplay(eventsOfTheDate)
+				
+			}
+		
+			limitEventsToDisplay();
+	
+		}, [eventsArray, eventsToFitDate])
+
+	// CALCULATE EVENTS TO FIT IN DATE SQUARE
+	useEffect(()=> {
+		let currentHeight;
+		const getDateSquareHeight = ()=> {
+			if(dateRef.current) {
+				currentHeight = dateRef.current.offsetHeight;	
+				const eventHeight = 24;				
+				const eventsToFit = Math.floor(currentHeight / (eventHeight + 16));
+				// TODO: RERENDERS ON EVERY RESIZE!!
+				if (eventsToFit !== eventsToFitDate) {
+					console.log(eventsToFit);
+					setEventsToFitDate(eventsToFit);
+				}					
+			} 
+		} 
+
+		getDateSquareHeight();
+		window.addEventListener("resize", getDateSquareHeight);
+
+		return () => window.removeEventListener("resize", getDateSquareHeight);
+	}, [])
+
 	// GET EVENTS OF THE DAY
-	const getAndSortEvents = (eventsArray: Event[]) => {
+	function getAndSortEvents(eventsArray: Event[]) {
 		const eventsOfTheDate: Event[] = eventsArray.filter((event: Event) => {
 			const eventDate = new Date(year, month, date).toLocaleDateString();		
 			return (
@@ -56,10 +98,12 @@ const DateOfMonth: React.FC<DateofMonthProps> = ({ year, month, date, dayClass, 
 		const sortedEventsArray = eventsOfTheDate.sort((a, b)=> 			
 			Number(a.startTime.replace(":","")) - Number(b.startTime.replace(":",""))
 		)
+		
 		return sortedEventsArray
 	}
 
-	const eventsOfTheDate = getAndSortEvents(eventsArray);
+
+	
 
 	const handleAddEvent = (year: number, month: number, date: number) => {
 		setModalOpen(true)
@@ -67,7 +111,7 @@ const DateOfMonth: React.FC<DateofMonthProps> = ({ year, month, date, dayClass, 
 	}
 
   return (
-	<div className={`day ${dayClass}`}>
+	<div ref={dateRef} className={`day ${dayClass}`}>
 		    <div className="day-header">
               <div className="week-name">{days[dayOfWeek]}</div>
               <div className={`day-number ${isToday && "today"}`}>{date}</div>
@@ -75,10 +119,11 @@ const DateOfMonth: React.FC<DateofMonthProps> = ({ year, month, date, dayClass, 
             </div>
 
 			<div className="events">
-				{eventsOfTheDate.map((event, index) => 
+				{eventsToDisplay.map((event, index) => 
 					<EventListing key={index} event={event} setIsEditing={setIsEditing} setModalOpen={setModalOpen}/>	
 				)}
 			</div>
+			{eventsOverflowed ? <button className="events-view-more-btn">+ {eventsOverflowed} more</button>: ""}
 	</div>
   )
 }
